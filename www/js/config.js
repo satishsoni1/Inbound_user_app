c46d26c7433ddbd5b5f813cce4ac682631ca2f38
satishsoni1 <satish.soni@globalspace.in>
2018-03-28 00:36:11 +0530

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://ec2-13-126-171-124.ap-south-1.compute.amazonaws.com/kmrs/mobileapp/api",
+	'ApiUrl2' : "http://ec2-13-126-171-124.ap-south-1.compute.amazonaws.com/Inbound_Project/webservice",
+	'DialogDefaultTitle' : "Inbound",
+	'pushNotificationSenderid' : "281792327024",
+	'facebookAppId' : "299608633836956",
+	'APIHasKey' : ""
+};
